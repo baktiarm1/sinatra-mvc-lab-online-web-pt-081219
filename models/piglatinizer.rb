340959156b0d4118905be911522f7df040420e8f
Baktiar Miah <baktiar.m@gmail.com>
2019-12-01 17:46:04 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,14 +10,19 @@ class PigLatinizer
  def is_vowel?(word)
    vowels = 'aeiouAEIOU'
    vowels.include?(word[0])
- end 
-
- def translate
-   if text.start_with?('a','e','i','o','u')
-    text << 'way'
-   else
-    text<<'ay'
-  end
  end
+
+ def piglatinzie(word)
+   if self.is_vowel?(word)
+     word.concat('way')
+   else
+     word.concat(word.slice!(/^[^aeiouAEIOU]*/i || "")) + 'ay'
+    end
+  end
+
+  def to_pig_lating(text)
+    text.split.collect {|word| piglatinize(word)}.join(' ')
+  end 
+
 
 end
